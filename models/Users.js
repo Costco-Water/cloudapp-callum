c20e02ee492cb3b4b64bf2900282b3280d6d19c8
Costco-Water <turnbull1995@gmail.com>
2025-05-28 19:39:33 +0000

    fix to run site

--- models/Users.js
+++ models/Users.js
@@ -1,5 +1,9 @@
+const mongoose = require("mongoose");
+
 const userSchema = new mongoose.Schema({
-    username: { type: String, required: true, unique: true },
+    username: { type: String, required: true },
     password: { type: String, required: true },
     role: { type: String, enum: ['admin', 'user'], default: 'user' }
 });
+
+module.exports = mongoose.model("User", userSchema);
